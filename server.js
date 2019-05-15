fa69e826479a4d9028a8442ddb15b8bd7acd76ed
CoraCodrean <50665097+CoraCodrean@users.noreply.github.com>
2019-05-15 20:04:23 +0300

    changng response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const db = knex({
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/',(req,res) => { res.send('it is working') })
+app.get('/',(req,res) => { res.send("it's working") })
 
 // app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)}); 
 // OR
