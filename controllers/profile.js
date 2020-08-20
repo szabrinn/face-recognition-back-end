8eb3b7630b79629ef90ff16c01959d3594598702
CoraCodrean <50665097+CoraCodrean@users.noreply.github.com>
2020-08-21 01:00:37 +0300

    fix indentation

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,18 +1,18 @@
 const handleProfile = (db) => (req, res) => {
-	const { id } = req.params;
-	db.select('*').from('users').where({id})
-	.then(user =>{
-
-		if(user.length){
-			res.json(user[0]);
-		} else {
-			res.status(400).json('user not found')
-		}
-		
-	})
-	.catch(err => res.status(400).json('error user'))
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("user not found");
+      }
+    })
+    .catch((err) => res.status(400).json("error user"));
 };
 
 module.exports = {
-	handleProfile 
-}
+  handleProfile,
+};
--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-const express = require( 'express');
+const express = require("express");
 
-const bodyParser = require('body-parser');
-const bcrypt = require ('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register.js');
-const signin = require('./controllers/signin.js');
-const profile = require('./controllers/profile.js');
-const image = require('./controllers/image.js');
+const register = require("./controllers/register.js");
+const signin = require("./controllers/signin.js");
+const profile = require("./controllers/profile.js");
+const image = require("./controllers/image.js");
 
 const app = express();
 const db = knex({
-	client: 'pg',
-	connection: {
-	connectionString : process.env.DATABASE_URL,
-	ssl: true,
-   	
-  }
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
 });
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/',(req,res) => { res.send("it's working") })
+app.get("/", (req, res) => {
+  res.send("it's working");
+});
 
-app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post("/signin", signin.handleSignin(db, bcrypt));
 
-app.post('/register', register.handleRegister(db, bcrypt));
+app.post("/register", register.handleRegister(db, bcrypt));
 
-app.get('/profile/:id', profile.handleProfile(db));
+app.get("/profile/:id", profile.handleProfile(db));
 
-app.put('/image', image.handleImage(db));
+app.put("/image", image.handleImage(db));
 
-app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-
-app.listen(process.env.PORT, 3000,()=> {
-	console.log(`app is running on port ${process.env.PORT}`);
-})
-
-// process.setMaxListeners(0);
+app.listen(process.env.PORT, 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+});
